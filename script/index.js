ed0b60a945434f42515e4319e3124843e1f69798
shoumik saha <you@example.com>
2025-06-16 21:18:35 +0600

    more add

--- script/index.js
+++ script/index.js
@@ -63,6 +63,43 @@ const loadcategoriesVideos = (id) => {
 
 }
 
+const loadVideoDetails = (videoid) => {
+    console.log(videoid);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoid}`
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => displayVideoDetails(data.video))
+
+
+};
+
+ const displayVideoDetails = (video) => {
+    console.log(video)
+    document.getElementById("video_details").showModal();
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML = `
+    
+    <div class="card bg-base-100 image-full  shadow-sm">
+        <figure>
+            <img w-full 
+            src="${video.thumbnail}"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="">${video.title}</h2>
+            <p>${video.description}</p>
+            <div class="card-actions justify-end">
+           
+            </div>
+        </div>
+        </div>
+    
+    `
+
+ }
+
+
+
 function displayCategoris(categories){
     
     const categoriesContainer = document.getElementById("category-container");
@@ -119,11 +156,14 @@ const displayvideos = (videos) => {
                 </div>
                 <div class="into">
                 <h2 class="text-sm font-semibold">Midnight Serenade</h2>
-                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} 
+                ${video.authors[0].verified  == true ? ` `: `` }
+                
                 <p class="text-sm text-gray-400">${video.others.views}</p>
                 </div>   
                 
             </div>
+            <button onclick =loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button>
             </div>  
         `
         videocontainer.append(videoCard)
